8b8f7ef262a4ef90238ff02db7c606a8a2f76e1e
EdwardGoomba <ask@edwardvdesign.com>
2018-01-02 15:21:30 -0500

    Added base of flickr AJAX request

--- js/flickr.js
+++ js/flickr.js
@@ -2,6 +2,21 @@ $(document).ready(function () {
   $('button').click(function () {
     $('button').removeClass('selected');
     $(this).addClass('selected');
+
+    // Send AJAX request to flickr
+    var flickerAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+    var animal = $(this).text();
+    var flickrOptions = {
+      tags: animal,
+      format: "json"
+    };
+
+    function displayPhotos(data) {
+
+    }
+
+    $.get(flickerAPI, flickrOptions, displayPhotos);
+
   });
 
 
