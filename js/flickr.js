798e0c6d53e736f787a67fcb5f6b3a41bac54bcf
EdwardGoomba <ask@edwardvdesign.com>
2018-01-02 23:46:13 -0500

    AJAX request complete

--- js/flickr.js
+++ js/flickr.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     $(this).addClass('selected');
 
     // Send AJAX request to flickr
-    var flickerAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+    var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
     var animal = $(this).text();
     var flickrOptions = {
       tags: animal,
@@ -12,7 +12,15 @@ $(document).ready(function () {
     };
 
     function displayPhotos(data) {
+      var photoHTML = '<ul>';
+      $.each( data.items, function (i, photo) {
+        photoHTML += '<li class="grid-25 tablet-grid-50">';
+        photoHTML += '<a href="' + photo.link + '" class="image">';
+        photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+      });
 
+      photoHTML += '</ul>';
+      $('#photos').html(photoHTML);
     }
 
     $.get(flickerAPI, flickrOptions, displayPhotos);
